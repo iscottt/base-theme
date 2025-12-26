d327e26a54e005e26a7c5f2085c32fb4c700e74a
MuMoo <iscottt@163.com>
2025-12-26 15:32:22 +0800

    feat: 404

--- app/src/pages/404.vue
+++ app/src/pages/404.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="four04 flex flex-col justify-center items-center">
-		<div class="title">Lost</div>
-		<p>放眼望去，这里一片荒芜，只剩白雪茫茫……<br>疲惫的旅人，您迷路了吗？</p>
-		<p><button @click="router.replace('/')">带我回家</button></p>
+		<h1 class="text-4xl font-bold">404</h1>
+		<p class="text-xl">Page not found</p>
+		<button @click="router.push('/')">Go back home</button>
 	</div>
 </template>
 
